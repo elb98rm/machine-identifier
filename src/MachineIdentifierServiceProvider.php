2b41ebdd054970a0107436318786188d3c91f0f7
Rick Morice <rick@floor9design.com>
2018-01-17 11:30:30 +0000

    Ongoing work on laravel composer support.

---
+++ src/MachineIdentifierServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Floor9design\MachineIdentifier;
+
+use Illuminate\Support\ServiceProvider;
+
+class MachineIdentifierServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
